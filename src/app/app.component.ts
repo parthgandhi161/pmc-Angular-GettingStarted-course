f8d150caa717c9e026e835bd98f9ba9e62d26fb6
parth <parthgandhi601@gmail.com>
2021-09-20 12:56:09 +0530

    Module 8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductService } from "./products/product.service";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "pm-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [ProductService],
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  title = "Angular: Getting Started";
 }
