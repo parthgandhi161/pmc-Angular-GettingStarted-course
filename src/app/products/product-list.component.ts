4b3c6f1a99ee529d7043a568a57a73760648d2be
parth <parthgandhi601@gmail.com>
2021-09-20 11:19:35 +0530

    Module 5

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,10 @@ import { Component } from "@angular/core";
 })
 export class ProductListComponent {
   pageTitle: string = "Product List";
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
+  listFilter: string = "cart";
   products: any[] = [
     {
       productId: 1,
@@ -63,4 +67,8 @@ export class ProductListComponent {
         "http://openclipart.org/image/300px/svg_to_png/120337/xbox-controller_01.png",
     },
   ];
+
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
 }
